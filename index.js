cabbd1b5c9cc95dbdfd4a228de96cfacf4d2874a
Eliel Cirino <elielscirino@gmail.com>
2023-02-21 16:02:26 -0300

    import userRoute

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const userRoute = require('./routes/userRoute')
+
 const app = express()
+app.use(bodyParser.urlencoded({extended: false}))
 const port = 3000
 
 app.get('/', (req, res) => res.send('Express'))
+userRoute(app)
 
 app.listen(port, () => console.log('localhost:3000'))
